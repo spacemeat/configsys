ebef39d0b8be99de8a9e67600a7cfab49f9f368a
Trevor Schrock <spacemeat@gmail.com>
2024-01-23 18:08:25 -0800

    Added keygen option for settin gup github SSH, etc.

--- linux/setup_github_ssh.sh
+++ linux/setup_github_ssh.sh
@@ -3,7 +3,7 @@
 if [ -e ~/.ssh/id_ed25519.pub ] ; then
 	echo "~/.ssh/id_ed25519.pub is already."
 	exit 0
-
+fi
 run 'ssh-keygen -t ed25519 -C "spacemeat@gmail.com"' || exit $?
 run 'eval "$(ssh-agent -s)"' || exit $?
 run 'ssh-add ~/.ssh/ed25519' || exit $?
