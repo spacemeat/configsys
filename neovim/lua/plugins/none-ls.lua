2f44b766af21a1cd59adb44a7a0aee7404b680c6
Trevor Schrock <spacemeat@gmail.com>
2024-03-04 10:21:09 -0800

    Updated nvim plugins.

--- neovim/lua/plugins/none-ls.lua
+++ neovim/lua/plugins/none-ls.lua
@@ -8,7 +8,6 @@ return {
 				null_ls.builtins.formatting.stylua,
 				-- js
 				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.diagnostics.eslint_d,
 				-- python
 				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.isort,
